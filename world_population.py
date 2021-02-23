c828fde23c9be0407749f3789da1ec372782a8c0
limonsafayet <limon6531@gmail.com>
2021-02-23 17:20:17 +0600

    working on  world population

--- world_population.py
+++ world_population.py
@@ -8,4 +8,15 @@ page = requests.get(url)
 
 soup = BeautifulSoup(page.text, 'lxml')
 
-print (soup)
+table = soup.find('table', class_ = 'table table-striped table-bordered table-hover table-condensed table-list')
+
+headers = []
+
+for i in table.find_all('th'):
+    title = i.text
+    headers.append(title)
+
+df = pd.DataFrame(columns= headers)
+print(df)
+
+
