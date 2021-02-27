b74f7f00de9800cb8d1bb699844dc9a876e87b91
limonsafayet <limon6531@gmail.com>
2021-02-27 13:31:15 +0600

    selenium

--- selenium_basics.py
+++ selenium_basics.py
@@ -1,4 +1,5 @@
-
+# Part 1
+"""
 from selenium import webdriver
 
 driver = webdriver.Chrome('/usr/bin/chromedriver')
@@ -15,7 +16,20 @@ for i in range(30):
     price = driver.find_element_by_xpath('//*[@id="'+str(i)+'"]/div[2]/div/p/span').text
 
     print(price)
+"""
 
+# Part 2
+# sending Text into an Input Box
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
+driver = webdriver.Firefox()
+driver.get('https://www.google.com/')
 
+box = driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input')
+
+box.send_keys('web scraping')
+box.send_keys(Keys.ENTER)
+
+# Clicking on a button
