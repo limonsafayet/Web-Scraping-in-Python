3a38a6bcfbd2cc5a7130fc849579bc08e720d4bb
limonsafayet <limon6531@gmail.com>
2021-02-27 13:35:47 +0600

    selenium button click

--- selenium_basics.py
+++ selenium_basics.py
@@ -1,4 +1,5 @@
 # Part 1
+
 """
 from selenium import webdriver
 
@@ -19,7 +20,6 @@ for i in range(30):
 """
 
 # Part 2
-# sending Text into an Input Box
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -27,9 +27,14 @@ from selenium.webdriver.common.keys import Keys
 driver = webdriver.Firefox()
 driver.get('https://www.google.com/')
 
+# sending Text into an Input Box
+
 box = driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input')
 
 box.send_keys('web scraping')
-box.send_keys(Keys.ENTER)
+#box.send_keys(Keys.ENTER) # Press Enter
 
 # Clicking on a button
+
+button = driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[2]/div[1]/div[3]/center/input[1]')
+button.click() # Press Search Button
