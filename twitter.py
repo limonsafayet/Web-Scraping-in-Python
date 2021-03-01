8b42f2a44c4e5cc68fdb5ecea5050a3502007f9d
limonsafayet <limon6531@gmail.com>
2021-03-01 19:09:55 +0600

    twitter complete

--- twitter.py
+++ twitter.py
@@ -6,15 +6,47 @@ import time
 
 driver = webdriver.Firefox()
 driver.get('https://twitter.com/login')
-
+time.sleep(5)
 username = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[1]/label/div/div[2]/div/input')
-username.send_keys('traideas')
+username.send_keys('user_name')
 
 password = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/main/div/div/div[2]/form/div/div[2]/label/div/div[2]/div/input')
-password.send_keys('')
+password.send_keys('password')
 
 login_button = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[3]/div').click()
 
 time.sleep(10)
 
+celebrity = 'sakib al hasan'
+search = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[1]/div/div/div[2]/input')
 
+search.send_keys(celebrity)
+search.send_keys(Keys.ENTER)
+time.sleep(5)
+people_button = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[1]/div[2]/nav/div/div[2]/div/div[3]/a/div').click()
+time.sleep(5)
+celebrity_profile = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/section/div/div/div[1]/div/div/div/div[2]/div[1]/div[1]/a/div/div[1]/div[1]/span').click()
+
+time.sleep(2)
+
+
+
+soup = BeautifulSoup(driver.page_source, 'lxml')
+
+postings = soup.find_all('div', class_='css-901oao r-1fmj7o5 r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-bnwqim r-qvutc0')
+
+tweets = []
+
+while True:
+    for post in postings:
+        tweets.append(post.text)
+    driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+    time.sleep(2)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    postings = soup.find_all('div',class_='css-901oao r-1fmj7o5 r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-bnwqim r-qvutc0')
+    tweets_final = list(set(tweets))
+    if len(tweets) > 200:
+        break
+
+
+print(tweets_final)
