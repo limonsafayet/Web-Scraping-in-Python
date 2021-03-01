b1f18d618779e358ad57e5270241205b318520f1
limonsafayet <limon6531@gmail.com>
2021-03-01 20:54:31 +0600

    indeed complete

--- indeed.py
+++ indeed.py
@@ -12,7 +12,7 @@ input_job_name.send_keys('data analyst')
 
 input_location = driver.find_element_by_xpath('//*[@id="text-input-where"]')
 input_location.send_keys('New York, NY')
-
+time.sleep(5)
 find_job = driver.find_element_by_xpath('/html/body/div/div[2]/div[3]/div[1]/div/div/div/form/div[3]').click()
 
 # Creates a dataframe
